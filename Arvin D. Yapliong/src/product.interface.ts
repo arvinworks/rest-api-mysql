f7bc2819883a963c6a48b8d234a1927bbca58ebc
unknown <YY147@ccs.local>
2024-03-01 12:37:15 +0800

    yapliong the great

---
+++ Arvin D. Yapliong/src/product.interface.ts
@@ -0,0 +1,16 @@
+export interface Product {
+    name : string,
+    price : number;
+    quantity : number;
+    image : string;
+
+}
+
+export interface UnitProduct extends Product {
+    id : string
+}
+
+export interface Products {
+    [key : string] : UnitProduct
+}
+
